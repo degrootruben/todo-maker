bc5e194363e48246847afd226c42342c69d91a94
degrootruben <rpg.degroot@gmail.com>
2020-09-19 23:19:46 +0200

    Deleted tabbed content feature because it was useless, also trying to fix auth features

--- public/frontendTaskLogic.js
+++ public/frontendTaskLogic.js
@@ -111,23 +111,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
     });
-
-    // Display contact or about tab
-    const tabs = document.querySelector(".tabs");
-    const panels = document.querySelectorAll(".panel");
-
-    tabs.addEventListener("click", e => {
-        if (e.target.tagName == "LI") {
-            const targetPanel = document.querySelector(e.target.dataset.target);
-
-            panels.forEach(panel => {
-                if (panel == targetPanel) {
-                    panel.classList.add("active");
-                } else {
-                    panel.classList.remove("active");
-                }
-            })
-        }
-    });
 });
 
