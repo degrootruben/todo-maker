267040ccc81a11970dcde608902691e5cc7b525e
degrootruben <rpg.degroot@gmail.com>
2020-09-19 17:41:11 +0200

    Adding error handling for signup data

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,26 @@
 const util = require("../util");
 const User = require("../models/User");
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: "", password: "" };
+  
+    // Duplicate email error
+    if (err.code === 11000) {
+      errors.email = "That email is already registered";
+      return errors;
+    }
+  
+    // Validation errors
+    if (err.message.includes("user validation failed")) {
+      Object.values(err.errors).forEach(({ properties }) => {
+        errors[properties.path] = properties.message;
+      });
+    }
+  
+    return errors;
+  }
+
 module.exports.login_get = (req, res) => {
     res.status(200).sendFile(util.getPath("views/login.html"));
 }
@@ -22,7 +42,7 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user);
     } 
     catch (err) {
-        console.log(err);
-        res.status(400).send("Error while signing up");
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
     }
 }
--- models/User.js
+++ models/User.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, "Please enter an email"],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, "Please enter a valid email"]
     },
     password: {
         type: String,
-        required: true,
-        minLength: 8
+        required: [true, "Please enter a password"],
+        minlength: [8, "Minimum password length is 8 characters"],
     }
-}, { timestamps: true });
+});
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
