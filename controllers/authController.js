2821e96c34a16ef5405bd063f6491cb5ca6ac5dd
degrootruben <rpg.degroot@gmail.com>
2020-09-19 17:12:43 +0200

    Added User model and made code for the signup post request

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,11 @@
 const util = require("../util");
+const User = require("../models/User");
 
 module.exports.login_get = (req, res) => {
     res.status(200).sendFile(util.getPath("views/login.html"));
 }
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
     res.send("User is trying to login");
     console.log(req.body);
 }
@@ -13,7 +14,15 @@ module.exports.signup_get = (req, res) => {
     res.status(200).sendFile(util.getPath("views/signup.html"));
 }
 
-module.exports.signup_post = (req, res) => {
-    res.send("New user signed up");
-    console.log(req.body);
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } 
+    catch (err) {
+        console.log(err);
+        res.status(400).send("Error while signing up");
+    }
 }
